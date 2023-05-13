7006f2c9cd7bc656cf6b7fedf6e2e90b089b910f
RawanHesham <97854538+RawanHesham20@users.noreply.github.com>
2023-05-13 04:35:51 +0300

    Add files via upload

---
+++ flow-detector-main/src/main.py
@@ -0,0 +1,27 @@
+# MMMMMMMM               MMMMMMMMIIIIIIIIIUUUUUUUU     UUUUUUU
+# M:::::::M             M:::::::MI::::::::IU::::::U     U::::::U
+# M::::::::M           M::::::::MI::::::::IU::::::U     U::::::U
+# M:::::::::M         M:::::::::MII::::::IIUU:::::U     U:::::UU
+# M::::::::::M       M::::::::::M  I::::I   U:::::U     U:::::U 
+# M:::::::::::M     M:::::::::::M  I::::I   U:::::D     D:::::U 
+# M:::::::M::::M   M::::M:::::::M  I::::I   U:::::D     D:::::U 
+# M::::::M M::::M M::::M M::::::M  I::::I   U:::::D     D:::::U 
+# M::::::M  M::::M::::M  M::::::M  I::::I   U:::::D     D:::::U 
+# M::::::M   M:::::::M   M::::::M  I::::I   U:::::D     D:::::U 
+# M::::::M    M:::::M    M::::::M  I::::I   U:::::D     D:::::U 
+# M::::::M     MMMMM     M::::::MII::::::IINU:::::UUUUUU:::::U 
+# M::::::M               M::::::MI::::::::IU:::::::::::::::UU  
+# M::::::M               M::::::MI::::::::IU:::::::::::::::UU  
+# M::::::M               M::::::MIIIIIIIIIUUUUUUUUUUUUUUUUUUUU  
+
+
+import asyncio
+
+
+async def run_gui():
+    import guisia
+    
+
+
+if __name__ in { "__main__", "__mp_main__" }:
+    asyncio.run(run_gui())
